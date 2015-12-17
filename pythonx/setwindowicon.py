84a65d5ea4959ef385012c0de85608b99b7b6c41
Charles <char101@ui.ac.id>
2015-12-17 13:11:45 +0700

    Add vim overlay to icon

--- pythonx/setwindowicon.py
+++ pythonx/setwindowicon.py
@@ -1,6 +1,9 @@
 import vim
+import os
+import hashlib
 from os import path
 from win32gui import GetActiveWindow, LoadImage, SendMessage, IMAGE_ICON, LR_LOADFROMFILE
+from PIL import Image
 
 ICON = 'gvim.ico'
 
@@ -8,15 +11,9 @@ WM_SETICON = 128
 ICON_SMALL = 0
 ICON_BIG = 1
 
-iconhandle = None
+OVERLAY = path.join(path.dirname(__file__), 'overlay.ico')
 
-def seticon():
-    global iconhandle
-    icon = findicon()
-    if icon:
-        iconhandle = LoadImage(0, icon, IMAGE_ICON, 16, 16, LR_LOADFROMFILE)
-        if iconhandle:
-            SendMessage(GetActiveWindow(), WM_SETICON, ICON_SMALL, iconhandle)
+iconhandle = None
 
 def findicon():
     dir = vim.eval("fnamemodify(getcwd(), ':p:h')")
@@ -28,3 +25,24 @@ def findicon():
         if parent == dir:
             break
         dir = parent
+
+def overlay(icon):
+    with open(icon, 'rb') as f:
+        hash = hashlib.md5(f.read()).hexdigest()
+    cache = path.join(os.environ['TEMP'], hash + '.ico')
+    if not path.isfile(cache):
+        bg = Image.open(icon)
+        fg = Image.open(OVERLAY)
+        Image.alpha_composite(bg, fg).save(cache)
+    return cache
+
+def seticon():
+    global iconhandle
+    if iconhandle is not None:
+        return
+    icon = findicon()
+    if icon:
+        icon = overlay(icon)
+        iconhandle = LoadImage(0, icon, IMAGE_ICON, 16, 16, LR_LOADFROMFILE)
+        if iconhandle:
+            SendMessage(GetActiveWindow(), WM_SETICON, ICON_SMALL, iconhandle)
